a75412a553f00c3143ee898101a3ef5dc21eac92
Carlos <carlos.anezeh@gmail.com>
2022-01-25 12:12:15 -0600

    Add main title and new fonts from google.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="container">
+      <header>
+        <h1>Week Expense</h1>
+      </header>
     </div>
   );
 }
