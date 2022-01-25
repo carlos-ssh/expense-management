f4e7d608e099093f849d939f5c4f6a07438f21c0
Carlos <carlos.anezeh@gmail.com>
2022-01-25 15:49:34 -0600

    Add a function that controlls the state when you set a number on the input.

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,14 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+  // Definir el state:
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, definirError] = useState(false);
+  // Funcion que lee el presupuesto
+  const definirPresupuesto = e => {
+    guardarCantidad( parseInt(e.target.value, 10) );
+  }
+
+  // Submit para definir presupuesto
+  const agregarPresupuesto = e => {
+    // Validar
+    
+    // Si se pasa la validacion
+  }
+
   return (
     <Fragment>
       <h2>Set your Income:</h2>
-      <form>
+      <form
+        onSubmit={agregarPresupuesto}
+      >
         <input
           type="number"
           className="u-full-width"
           placeholder="$500"
+          onChange={definirPresupuesto}
         />
 
         <input
