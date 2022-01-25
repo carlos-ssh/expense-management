5ce352c8b41d3df8854f410432e5f7f63b2a4714
Carlos <carlos.anezeh@gmail.com>
2022-01-25 16:54:12 -0600

    Add a error message using a ternary function.

--- src/components/Forlulario.js
+++ src/components/Forlulario.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
+import Error from './Error';
 
 const Formulario = () => {
   const [nombre, guardarNombre] = useState('');
   const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
 
   // Cuando el usuario agrega un gasto
   const agregarGasto = e => {
     e.preventDefault();
 
     // Validar
-
+    if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
     // Construir el gasto
-
+    
     // Pasar el gasto al componente principal
 
     // Resetear el formulario
-    
+
   }
 
   return (
@@ -23,6 +29,7 @@ const Formulario = () => {
       onSubmit={agregarGasto}
     >
       <h2>Set your data here!</h2>
+      { error ? <Error mensaje="Missing to fill inputs or incorrect expense" /> : null }
       <div className="campo">
         <label className="campo">Expense Name</label>
         <input
